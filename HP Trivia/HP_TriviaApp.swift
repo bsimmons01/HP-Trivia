4d5755554bc1b099a467b1cacddefd41b023d37e
Brian Simmons <bsimmons01@gmail.com>
2025-03-08 15:13:57 -0700

    Initial Commit

---
+++ HP Trivia/HP_TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HP_TriviaApp.swift
+//  HP Trivia
+//
+//  Created by Brian Simmons on 3/8/25.
+//
+
+import SwiftUI
+
+@main
+struct HP_TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
