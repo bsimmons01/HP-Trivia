441dbd2b6d4be54973c42026bf88f81bce0248d7
Brian Simmons <bsimmons01@gmail.com>
2025-03-09 12:57:02 -0700

    Build Game Instructions screen

--- HP Trivia/Constants.swift
+++ HP Trivia/Constants.swift
@@ -6,7 +6,30 @@
 //
 
 import Foundation
+import SwiftUI
 
 enum Constants {
     static let hpFont = "PartyLetPlain"
+    
+}
+
+struct InfoBackgroundImage: View {
+    var body: some View {
+        
+        Image("parchment")
+            .resizable()
+            .ignoresSafeArea()
+            .background(.brown)
+    }
+}
+
+extension Button {
+    @MainActor func doneButton() -> some View {
+        self
+            .font(.largeTitle)
+            .padding()
+            .buttonStyle(.borderedProminent)
+            .tint(.brown)
+            .foregroundColor(.white)
+    }
 }
